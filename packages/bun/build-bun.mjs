17408ae3e4a5917303272bba950c98119b2ca84e
Chris Roth <chris@cjroth.com>
2026-03-15 20:27:28 -0400

    chore: publish v0.2.37

--- packages/bun/build-bun.mjs
+++ packages/bun/build-bun.mjs
@@ -24,6 +24,7 @@ const nativeFiles = new Set([
   "renderer.ts",
   "console.ts",
   "NativeSpanFeed.ts",
+  "lib/singleton.ts",
   "lib/stdin-buffer.ts",
   "lib/bunfs.ts",
   "lib/output.capture.ts",
